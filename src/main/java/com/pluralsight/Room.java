11d6267fee1aa8a095d4761538dcdbc916296117
BogdanATA <batameev@gmail.com>
2026-05-04 13:14:53 -0400

    Room class attributes and getters created

--- src/main/java/com/pluralsight/Room.java
+++ src/main/java/com/pluralsight/Room.java
@@ -1,4 +1,34 @@
 package com.pluralsight;
 
 public class Room {
+    // attributes
+    private int numBeds;
+    private double price;
+    private boolean isOccupied;
+    private boolean isDirty;
+    private boolean isAvailable;
+
+
+
+
+    // getters
+    public int getNumBeds() {
+        return numBeds;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public boolean isOccupied() {
+        return isOccupied;
+    }
+
+    public boolean isDirty() {
+        return isDirty;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
 }
